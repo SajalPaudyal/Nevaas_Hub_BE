b7c9717751060774d1bcd33927a2efbb94383c38
sajalpaudyal <paudyalsajal@gmail.com>
2025-12-25 00:31:52 +0100

    updated index.js routes

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-// 1. Path includes "./src/"
-// 2. Extension MUST be ".js" for NodeNext compatibility
 import { upload } from "./src/middlewares/upload.js";
 import { register } from "./src/controllers/auth.controller.js";
 import { login } from "./src/controllers/login.controller.js";
@@ -20,24 +18,7 @@ app.get("/", (req, res) => {
   res.send("NevaasHub API is working");
 });
 
-app.get("/test-db", async (req, res) => {
-    try {
-        console.log("🔍 Testing Database Connection...");
-        
-        // This runs a simple raw SQL query: SELECT 1 + 1
-        const result = await db.execute(sql`SELECT 1 + 1 AS result`);
-        
-        console.log("✅ Database Response:", result);
-        res.json({ message: "Database is connected!", data: result });
-    } catch (error: any) {
-        console.error("❌ Database Connection Error:", error);
-        res.status(500).json({ 
-            message: "Database connection failed", 
-            error: error.message 
-        });
-    }
-});
-// Authentication Routes
+
 app.post("/api/auth/register", upload.single("idCard"), register);
 app.post("/api/auth/login", login);
 
